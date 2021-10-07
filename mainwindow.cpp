de4d84cf29907380a69ed6a927ab647868e6f246
avttrue <truetrifonov@yandex.ru>
2021-10-07 17:39:58 +0300

    edit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,22 +10,6 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    QFile file("/home/trifonovav/Projects_QT/Viola/test/Bankrobber.mp3");
-    if(!file.open(QIODevice::ReadOnly))
-        qDebug() << "File not opened";
-    qDebug() << "File size:" << file.size(); // File size: 11181085
-
-    QByteArray ba = file.readAll();
-    qDebug() << "ByteArray size:" << ba.size(); // ByteArray size: 11181085
-    /* // ByteArray Buffer variant
-    QBuffer* buffer = new QBuffer(this);
-    buffer->setData(ba);
-    if(!buffer->open(QIODevice::ReadOnly))
-        qDebug() << "Buffer not opened";
-    qDebug() << "Buffer size:" << buffer->size(); // Buffer size: 11181085
-
-    buffer->seek(qint64(0));
-    */
     auto audioOutput = new QAudioOutput(this);
     auto player = new QMediaPlayer(this);
     player->setAudioOutput(audioOutput);
@@ -42,6 +26,23 @@ MainWindow::MainWindow(QWidget *parent)
     QObject::connect(player, &QMediaPlayer::playbackStateChanged,
                      [=](QMediaPlayer::PlaybackState state)
     { qDebug() << "PlaybackState:" << state; });
+
+    QFile file("../test/Bankrobber.mp3");
+    if(!file.open(QIODevice::ReadOnly))
+        qDebug() << "File not opened";
+    qDebug() << "File size:" << file.size(); // File size: 11181085
+
+    QByteArray ba = file.readAll();
+    qDebug() << "ByteArray size:" << ba.size(); // ByteArray size: 11181085
+    /* // ByteArray Buffer variant
+    QBuffer* buffer = new QBuffer(this);
+    buffer->setData(ba);
+    if(!buffer->open(QIODevice::ReadOnly))
+        qDebug() << "Buffer not opened";
+    qDebug() << "Buffer size:" << buffer->size(); // Buffer size: 11181085
+
+    buffer->seek(qint64(0));
+    */
 
     // TemporaryFile variant
     QTemporaryFile tfile;
